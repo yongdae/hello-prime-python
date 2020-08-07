e86faac658b77feef4b28bc362b7fb2054cd381e
Yongdae <7942yongdae@gmail.com>
2020-08-07 11:24:47 +0900

    styles: 공백 수정

--- ch3/RadiusCalculator.py
+++ ch3/RadiusCalculator.py
@@ -1,5 +1,5 @@
 # 반지름 입력
-radius=eval(input("반지름 값을 입력하세요:"))
+radius = eval(input("반지름 값을 입력하세요:"))
 
 print("넓이는 ", radius * radius * 3.141592, " 입니다.")
 print("둘레는 ", 2 * radius * 3.141592, " 입니다.")
